358c6704e17ba882ee48b860dd733812066ea851
ed4becky <ed4becky@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-02-18 19:03:27 +0000

    nicer tools page
    git-svn-id: https://plugins.svn.wordpress.org/rootspersona/trunk@347465 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- rootsToolsPage.php
+++ rootsToolsPage.php
@@ -16,19 +16,19 @@ echo "<p style='padding: .5em; background-color: red; color: white; font-weight:
 echo  "<table class='form-table'>";
 
 echo  "<tr style='vertical-align: top'>";
-echo  "<td><div class='rp_linkbutton'><a href=' " . site_url() . "?page_id=" . get_option('rootsCreatePage') . "'>" . __('Add Uploaded Persons') . "</a></div></td>";
+echo  "<td style='width:200px;'><div class='rp_linkbutton'><a href=' " . site_url() . "?page_id=" . get_option('rootsCreatePage') . "'>" . __('Add Uploaded Persons') . "</a></div></td>";
 echo  "<td style='vertical-align:middle'>" . __('Review the list of people you have uploaded but not created pages for.') . "</td></tr>";
 
 echo  "<tr style='vertical-align: top'>";
-echo  "<td><div class='rp_linkbutton'><a href=' " . site_url() . "?page_id=" . get_option('rootsUploadGedcomPage') . "'>" . __('Upload GEDCOM') . "</a></div></td>";
+echo  "<td style='width:200px;'><div class='rp_linkbutton'><a href=' " . site_url() . "?page_id=" . get_option('rootsUploadGedcomPage') . "'>" . __('Upload GEDCOM') . "</a></div></td>";
 echo  "<td style='vertical-align:middle'>" . __('Upload (or re-upload) a gedcom file.') . "</td></tr>";
 
 echo  "<tr style='vertical-align: top'>";
-echo  "<td><div class='rp_linkbutton'><a href=' " . site_url() . "?page_id=" . get_option('rootsIncludePage') . "'>" . __('Review Excluded Persons') . "</a></div></td>";
+echo  "<td style='width:200px;'><div class='rp_linkbutton'><a href=' " . site_url() . "?page_id=" . get_option('rootsIncludePage') . "'>" . __('Review Excluded Persons') . "</a></div></td>";
 echo  "<td style='vertical-align:middle'>" . __('Review people you have previous excluded, and include the ones you select.') . "</td></tr>";
 
 echo  "<tr style='vertical-align: top'>";
-echo  "<td><div class='rp_linkbutton'><a href=' " . site_url() . "?page_id=" . get_option('rootsPersonaIndexPage') . "'>" . __('Name Index') . "</a></div></td>";
+echo  "<td style='width:200px;'><div class='rp_linkbutton'><a href=' " . site_url() . "?page_id=" . get_option('rootsPersonaIndexPage') . "'>" . __('Name Index') . "</a></div></td>";
 echo  "<td style='vertical-align:middle'>" . __('View a sortable index table of all persons that you have created pages for.') . "</td></tr>";
 
 echo  "</table></div>";
