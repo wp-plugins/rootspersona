130e560c6ea78468ea148f3d25f62169b86291f2
ed4becky <ed4becky@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-02-18 23:16:29 +0000

    git-svn-id: https://plugins.svn.wordpress.org/rootspersona/trunk@347525 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- php/personUtility.php
+++ php/personUtility.php
@@ -648,12 +648,15 @@ class PersonUtility {
                 $xp = new XsltProcessor();
                 // create a DOM document and load the XSL stylesheet
                 $xsl = new DomDocument;
-                $xsl->load($pluginDir . 'xsl/personaIndex.xsl');
+                if(isset($atts["xsl"]))
+                	$xslFile = $atts["xsl"];
+                if(!isset($xslFile) || $xslFile == '')
+                	$xslFile = $pluginDir . 'xsl/personaIndex.xsl';                
 
                 // import the XSL stylesheet into the XSLT process
                 $xp->importStylesheet($xsl);
                 $xp->setParameter('','site_url',$mysite);
-                $xp->setParameter('','data_dir','../../../../' . $dataDir);
+                $xp->setParameter('','data_dir',strtr(ABSPATH,'\\','/') . $dataDir);
 
                 
                 // create a DOM document and load the XML data
