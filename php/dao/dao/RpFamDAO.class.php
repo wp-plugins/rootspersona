2cbf15cfb8fccf35255e175dbeb950d28e866ff0
ed4becky <ed4becky@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-04-03 22:58:57 +0000

    git-svn-id: https://plugins.svn.wordpress.org/rootspersona/branches/2.0.0@368529 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ php/dao/dao/RpFamDAO.class.php
@@ -0,0 +1,89 @@
+<?php
+/**
+ * Intreface DAO
+ *
+ * @author: http://phpdao.com
+ * @date: 2011-04-03 18:56
+ */
+interface RpFamDAO{
+
+	/**
+	 * Get Domain object by primry key
+	 *
+	 * @param String $id primary key
+	 * @Return RpFam 
+	 */
+	public function load($id, $batchId);
+
+	/**
+	 * Get all records from table
+	 */
+	public function queryAll();
+	
+	/**
+	 * Get all records from table ordered by field
+	 * @Param $orderColumn column name
+	 */
+	public function queryAllOrderBy($orderColumn);
+	
+	/**
+ 	 * Delete record from table
+ 	 * @param rpFam primary key
+ 	 */
+	public function delete($id, $batchId);
+	
+	/**
+ 	 * Insert record to table
+ 	 *
+ 	 * @param RpFam rpFam
+ 	 */
+	public function insert($rpFam);
+	
+	/**
+ 	 * Update record in table
+ 	 *
+ 	 * @param RpFam rpFam
+ 	 */
+	public function update($rpFam);	
+
+	/**
+	 * Delete all rows
+	 */
+	public function clean();
+
+	public function queryByRestrictionNotice($value);
+
+	public function queryBySpouse1($value);
+
+	public function queryByIndiBatchId1($value);
+
+	public function queryBySpouse2($value);
+
+	public function queryByIndiBatchId2($value);
+
+	public function queryByAutoRecId($value);
+
+	public function queryByGedChangeDate($value);
+
+	public function queryByUpdateDatetime($value);
+
+
+	public function deleteByRestrictionNotice($value);
+
+	public function deleteBySpouse1($value);
+
+	public function deleteByIndiBatchId1($value);
+
+	public function deleteBySpouse2($value);
+
+	public function deleteByIndiBatchId2($value);
+
+	public function deleteByAutoRecId($value);
+
+	public function deleteByGedChangeDate($value);
+
+	public function deleteByUpdateDatetime($value);
+
+
+}
+?>
---
+++ php/dao/dto/RpFam.class.php
@@ -0,0 +1,22 @@
+<?php
+	/**
+	 * Object represents table 'rp_fam'
+	 *
+     	 * @author: http://phpdao.com
+     	 * @date: 2011-04-03 18:55	 
+	 */
+	class RpFam{
+		
+		var $id;
+		var $batchId;
+		var $restrictionNotice;
+		var $spouse1;
+		var $indiBatchId1;
+		var $spouse2;
+		var $indiBatchId2;
+		var $autoRecId;
+		var $gedChangeDate;
+		var $updateDatetime;
+		
+	}
+?>
